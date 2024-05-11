c5d1a848ed0f527279c3cc59ee4fb99ee2c55e26
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-05-11 22:13:29 +0530

    create method of update delete view items

--- OnlineAuctionSystem/src/main/java/org/example/onlineauctionsystem/Controller/ItemController.java
+++ OnlineAuctionSystem/src/main/java/org/example/onlineauctionsystem/Controller/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -34,5 +35,46 @@ public class ItemController {
         Item newItem = itemService.addItem(item);
         return new ResponseEntity<>(newItem, HttpStatus.CREATED);
     }
+
+    //get item details from the id
+    @GetMapping("/items/{id}")
+    public ResponseEntity<Item> getItemDetailsById(@PathVariable Long id) {
+        Optional<Item> optionalItem = itemService.getItemById(id);
+        return optionalItem.map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    //update item details
+    @PutMapping("/updateitems/{id}")
+    public ResponseEntity<Item> updateItemDetails(@PathVariable Long id, @RequestBody Item updatedItemDetails) {
+        Optional<Item> optionalItem = itemService.getItemById(id);
+        if (optionalItem.isPresent()) {
+            Item itemToUpdate = optionalItem.get();
+            // Update item details with the new values from updatedItemDetails
+            itemToUpdate.setItemname(updatedItemDetails.getItemname());
+            itemToUpdate.setDescription(updatedItemDetails.getDescription());
+            itemToUpdate.setStartingPrice(updatedItemDetails.getStartingPrice());
+            itemToUpdate.setBidEndTime(updatedItemDetails.getBidEndTime());
+            itemToUpdate.setImageData(updatedItemDetails.getImageData());
+            // Save the updated item
+            Item updatedItem = itemService.updateItem(itemToUpdate);
+            return ResponseEntity.ok(updatedItem);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //delete item
+    @DeleteMapping("/deleteitem/{id}")
+    public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+        Optional<Item> optionalItem = itemService.getItemById(id);
+        if (optionalItem.isPresent()) {
+            itemService.deleteItem(id);
+            return ResponseEntity.ok("Item deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 }
